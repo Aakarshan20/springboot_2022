114d9b1fe2cc0db853e6acc63120b755d337789f
abc <abc@test.com>
2022-02-28 13:06:48 +0800

    update

--- src/main/java/com/atguigu/HelloWorldMainApplication.java
+++ src/main/java/com/atguigu/HelloWorldMainApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HelloWorldMainApplication {
     public static void main(String[] args) throws Exception{
+        // 勿忘args
         SpringApplication.run(HelloWorldMainApplication.class, args);
     }
 }
